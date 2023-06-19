dd53236c3e7e6cacfcfd2f4580b94063460d19d7
ranjith201098 <134181374+ranjith201098@users.noreply.github.com>
2023-06-18 20:10:24 -0700

    Add files via upload

---
+++ Schema.js
@@ -0,0 +1,19 @@
+/* const mongoose = require('mongoose');
+
+
+
+
+
+const ReactFormDataSchema = new mongoose.Schema({
+        name: {
+            type: String,
+           
+        },
+        phonenumber: {
+            type: Number,
+           
+        }
+    });
+    
+    const User = mongoose.model('User', ReactFormDataSchema);
+    module.exports = User; */
